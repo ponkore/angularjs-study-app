08adf52da0de81d23c063b769e15939ae026e8c8
ponkore <ponkore@gmail.com>
2018-04-18 00:02:18 +0900

    add datepicker sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ButtonsCheckboxComponent } from './components/buttons-checkbox/buttons-checkbox.component';
+import { NgbdDatepickerPopupComponent } from './components/ngbd-datepicker-popup/ngbd-datepicker-popup.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ButtonsCheckboxComponent
+        ButtonsCheckboxComponent,
+        NgbdDatepickerPopupComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/components/ngbd-datepicker-popup/ngbd-datepicker-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ngbd-datepicker-popup',
+    templateUrl: './ngbd-datepicker-popup.component.html',
+    styleUrls: ['./ngbd-datepicker-popup.component.scss']
+})
+export class NgbdDatepickerPopupComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
